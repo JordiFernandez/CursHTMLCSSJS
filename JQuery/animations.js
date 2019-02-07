4b9771a8cee02d398d8622371aa6ffe7db857806
JordiFernandez <36668960+JordiFernandez@users.noreply.github.com>
2019-02-07 12:33:44 +0100

    animations

--- JQuery/animations.js
+++ JQuery/animations.js
@@ -1,11 +1,41 @@
 var rotation = 0;
+var initheight = 0;
+var initwidth = 0;
 
-function rotate(degrees){
-	$('#1').css ({'transform' : 'rotate' + 'deg'});
-	return $('#1');
+function rotate2(degrees){
+	$('#primer').css ({'transform' : 'rotateY(' + degrees + 'deg)'});
+	return $('#primer');
 }
 
-$('#1').click(function(){
-	rotation += 5;
-	$('#1').rotate(rotation);
-})
+function update(){
+	rotation += 30;
+	rotate2(rotation);
+}
+window.onload = function(){
+	setInterval(update,1000);
+
+	$("#big").click(function(){
+			initwidth = $("#segon").width();
+			initheight = $("#segon").height();
+			initheight +=20;
+			initwidth += 50;
+		$("#segon").animate({
+
+			height: initheight+"px",
+			width: initwidth+"px"
+		}, 1000);
+	});
+
+	$("#small").click(function(){
+		initwidth = $("#segon").width();
+		initheight = $("#segon").height();
+		initheight -=20;
+		initwidth -= 50;
+		$("#segon").animate({
+			height: initheight+"px",
+			width: initwidth+"px"
+		}, 1000);
+	});
+
+}
+
